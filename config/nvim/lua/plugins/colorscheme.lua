6129992000cf9957d45937ba0893d3401167cf69
Shane <me@shanel.ee>
2026-08-08 14:53:55 +0100

    Gruvbox neovim

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,9 @@
 return {
-  "AlexvZyl/nordic.nvim",
+  "ellisonleao/gruvbox.nvim",
+  enabled = true,
   lazy = false,
   priority = 1000,
   config = function()
-    require("nordic").load()
+    require("gruvbox").load()
   end,
 }
